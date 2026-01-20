372a546b331f2c571e7b67a20e7594578a7a2e46
Angeliki Alexopoulou <angeliki.alexop@gmail.com>
2026-01-20 22:05:39 +0200

    set feature flags to false

--- src/feature_flags.js
+++ src/feature_flags.js
@@ -3,11 +3,11 @@
 const featureFlags = {
   // When true, the step-by-step decryption submit flow is enabled.
   // Toggle to false to disable the Submit button for Decryption mode.
-  enable_stepbystep_decryption: true,
+  enable_stepbystep_decryption: false,
   // When true, inverse practice exercises (InvShiftRows, InvSubBytes,
   // InvMixColumns) are shown on the Train page. If set to false,
   // those inverse exercises will be hidden.
-  enable_train_inverse_steps: true,
+  enable_train_inverse_steps: false,
 };
 
 export default featureFlags;
